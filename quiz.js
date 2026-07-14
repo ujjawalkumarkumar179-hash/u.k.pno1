b7b43c7773566bbebff0e7ce530485d206024458
ujjawalkumarkumar179-hash <ujjawalkumarkumar179@gmail.com>
2026-07-14 18:32:19 +0530

    Create quiz.js

---
+++ quiz.js
@@ -0,0 +1,57 @@
+let currentQuestion = 0;
+let score = 0;
+
+const questionBox = document.getElementById("question");
+const optionsBox = document.getElementById("options");
+const nextBtn = document.getElementById("nextBtn");
+const resultBox = document.getElementById("result");
+
+function loadQuestion() {
+    let q = questions[currentQuestion];
+
+    questionBox.innerHTML = 
+    (currentQuestion + 1) + ". " + q.question;
+
+    optionsBox.innerHTML = "";
+
+    q.options.forEach(option => {
+        let button = document.createElement("button");
+        button.innerHTML = option;
+
+        button.onclick = function () {
+            checkAnswer(option);
+        };
+
+        optionsBox.appendChild(button);
+    });
+}
+
+function checkAnswer(selected) {
+
+    let correct = questions[currentQuestion].answer;
+
+    if(selected === correct){
+        score++;
+    }
+
+    nextBtn.style.display = "block";
+}
+
+nextBtn.onclick = function(){
+
+    currentQuestion++;
+
+    if(currentQuestion < questions.length){
+        loadQuestion();
+        nextBtn.style.display = "none";
+    }
+    else{
+        questionBox.innerHTML = "Test Complete";
+        optionsBox.innerHTML = "";
+        resultBox.innerHTML = 
+        "आपका स्कोर: " + score + "/" + questions.length;
+        nextBtn.style.display = "none";
+    }
+};
+
+loadQuestion();
