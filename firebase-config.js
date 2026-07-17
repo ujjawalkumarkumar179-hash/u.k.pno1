93669cffd711def57554ff536b130c5e3d16c293
ujjawalkumarkumar179-hash <ujjawalkumarkumar179@gmail.com>
2026-07-17 12:56:27 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,12 @@
+// Firebase Configuration
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDv1tOYiuQAYAtOa85-9gZvM-fCrjWow6E",
+  authDomain: "ukp-no1-education.firebaseapp.com",
+  projectId: "ukp-no1-education",
+  storageBucket: "ukp-no1-education.firebasestorage.app",
+  messagingSenderId: "9661556950"
