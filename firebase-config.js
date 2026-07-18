c6a54ca64f13e0b5cc9bb58651a512d2a58a1bcf
ujjawalkumarkumar179-hash <ujjawalkumarkumar179@gmail.com>
2026-07-18 09:23:39 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -9,11 +9,9 @@ const firebaseConfig = {
   authDomain: "ukp-no1-education.firebaseapp.com",
   projectId: "ukp-no1-education",
   storageBucket: "ukp-no1-education.firebasestorage.app",
-  messagingSenderId: "983226412948
+  messagingSenderId: "983226412948",
     "
-  appId: "1:983226412948:web:7d332914d26850085ca981"
-};
-
+appId: "1:983226412948:web:7d332914d26850085ca981"
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
