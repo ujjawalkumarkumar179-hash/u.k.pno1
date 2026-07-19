0b3adcf6a06288fa69146063b3ba87d3ed579df4
ujjawalkumarkumar179-hash <ujjawalkumarkumar179@gmail.com>
2026-07-19 07:38:32 +0530

    Update certificate.js

--- certificate.js
+++ certificate.js
@@ -3,7 +3,7 @@
 // Certificate System - Part 1
 // ===============================
 
-export function showCertificate(currentUser.displayName, subject, testName, score, total);
+export function showCertificate(studentName, subject, testName, score, total) {
 
     const percentage = Math.round((score / total) * 100);
 
@@ -23,6 +23,16 @@ export function showCertificate(currentUser.displayName, subject, testName, scor
         title = "📚 KEEP PRACTICING";
         grade = "C";
     }
+
+    document.getElementById("certificateSection").style.display = "block";
+    document.getElementById("certificateTitle").textContent = title;
+    document.getElementById("studentName").textContent = studentName;
+    document.getElementById("studentSubject").textContent = subject;
+    document.getElementById("studentTest").textContent = testName;
+    document.getElementById("studentScore").textContent = `${score} / ${total}`;
+    document.getElementById("studentPercentage").textContent = percentage + "%";
+    document.getElementById("studentGrade").textContent = grade;
+}
 
     document.getElementById("certificateSection").style.display = "block";
 
